a3e8a6e954c64ea44ad6a854972264c7f76fe773
Torianyk <an.mol4anova@gmail.com>
2024-03-01 13:43:12 +0200

    Added the links on GitHub

--- src/App.js
+++ src/App.js
@@ -2,5 +2,32 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="Container">
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/torianykhanna"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Hanna Torianyk
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/torianykhanna/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a href="#" target="_blank" rel="noreferrer">
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
